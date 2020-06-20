3ee21a2cbe062e035b90ec4367032c7792476625
peterchege442@gmail.com <peterchege442@gmail.com>
2020-06-20 11:35:41 +0300

    why us content update

--- resources/views/pages/reasons.blade.php
+++ resources/views/pages/reasons.blade.php
@@ -26,7 +26,9 @@
                                 MOST PARENTS WISH THEY HAD A PERSONAL FINANCIAL HEAD START
                             </li>
                             <p>
-                                Imagine if you had been taught at a tender age about money and, more specifically, that the way to wealth is through cash flowing assets. How different would your life be today if you had learned these concepts at an early age?
+                                Imagine if you had been taught at a tender age about money and, more specifically, that the way to wealth is through cash flowing assets. 
+                                How different would your life be today if you had learned these concepts at an early age?
+
                             </p>
                         </div>
                         <div class="container">
@@ -34,7 +36,13 @@
                                 THE BUCK STOPS WITH THE YOU
                             </li>
                             <p>
-                                Today’s parents/caregivers need to understand that psychologically, the foundation of financial literacy is developed as early as age 3, mainly through a child observing their surroundings. A parent/caregiver who does not take charge to ensure the right behavior is developed, simply hands over that responsibility to other powerful forces such as media adverts to do that work for them. These adverts instill ‘’consumer” habits subconsciously. It has always been and will always be the parent’s/caregiver’s decisions, knowingly or unknowingly, that set this foundation. Click here to learn more on this
+                                Today’s parents/caregivers need to understand that psychologically, the foundation of financial literacy is developed as early as age 3, 
+                                mainly through a child observing their surroundings. Proof – if you walk into a shop with a 3-year-old, more often than not, they will 
+                                ask you to purchase something for them. They have already learnt that money is a commodity that is used to exchange for things they 
+                                want and this was through observation. Therefore, a parent/caregiver who does not take charge to ensure the right behavior is developed, 
+                                simply hands over that responsibility to other powerful forces such as media adverts to do that work for them. These adverts instill 
+                                ‘’consumer” habits subconsciously. It has always been and will always be the parent’s/caregiver’s decisions, knowingly or unknowingly, 
+                                that set this foundation. To learn more, read our blog – 1, 2, 3 and 4. 
                             </p>
                         </div>
                         <div class="container">
@@ -42,15 +50,20 @@
                                 DEVELOPING THE RIGHT FINANCIAL MINDSET
                             </li>
                             <p>
-                                Kids are like sponges. From the time they are babies, they absorb what they see and hear around them. They formulate their ideas about money from their environment and this develops into a habit. Without the right mindset at a tender age, children are likely to later struggle as adults to break away from the wrong mindset they developed.
+                                Kids are like sponges. From the time they are babies, they absorb what they see and hear around them. They formulate their ideas about 
+                                money from their environment and this develops into a habit. Without the right mindset at a tender age, children are likely to later 
+                                struggle as adults to break away from the wrong mindset they developed.
                             </p>
                         </div>
                         <div class="container">
                             <li>
-                                WE TEACH “HOW”TO THINK, NOT “WHAT”TO THINK
+                                WE TEACH “HOW” TO THINK, NOT “WHAT” TO THINK
                             </li>
                             <p>
-                                When we teach kids “A” is for apple, that is giving them an answer, then testing how well they remember that answer, in effect, we stifle their thinking and creativity. This is teaching them “what” to think. If we ask them to come up with words that start with “A”, we open up their thinking and creativity. This is teaching them “how” to think. Teaching them “how” to think opens the mind to realizing there are always options in any given situation. Our programs are designed to teach them “how” to think.
+                                When we teach kids “A” is for apple, that is giving them an answer, then testing how well they remember that answer, in effect, 
+                                we stifle their thinking and creativity. This is teaching them “what” to think. If we ask them to come up with words that start with 
+                                “A”, we open up their thinking and creativity. This is teaching them “how” to think. Teaching them “how” to think opens the mind to 
+                                realizing there are always options in any given situation. Our programs are designed to teach them “how” to think.
                             </p>
                         </div>
                         <div class="container">
@@ -58,7 +71,9 @@
                                 SIMULATING REAL LIFE EXAMPLES
                             </li>
                             <p>
-                                Our programs are designed to be hands-on, practical and engaging activities that simulate real-life financial matters. These include: savings, needs and wants, good and bad credit, assets, liabilities, cash-flow, generation of unique business ideas through problem solving, investing in different financial products etc.We make it highly practical
+                                Our programs are designed to be hands-on, practical and engaging activities that simulate real-life financial matters or real- life experiences. 
+                                These include: savings, needs and wants, good and bad credit, assets, liabilities, cash-flow, generation of unique business ideas through problem 
+                                solving, investing in different financial products etc. We make it highly practical. 
                             </p>
                         </div>
 
@@ -67,7 +82,9 @@
                                 BREAKING DOWN OF TECHNICAL FINANCIAL TERMS
                             </li>
                             <p>
-                                Terms such as assets, liabilities, cash flow, credit score and reports, are broken down to the audience’s level with ease of understanding and application being the main goal. Kids and teens enjoy fun activities so we make learning technical financial terms fun and entertaining which enables those lessons to stick in their minds.
+                                Terms such as assets, liabilities, cash flow, credit score and reports, are broken down to the audience’s level with ease of understanding and 
+                                application being the main goal. Kids and teens enjoy fun activities so we make learning technical financial terms fun and entertaining which enables 
+                                those lessons to stick in their minds.
                             </p>
                         </div>
                         <div class="container">
@@ -75,7 +92,9 @@
                                 THEY LEARN THE SECRET OF THE WEALTHY
                             </li>
                             <p>
-                                Before joining the programs, kids and teens normally have a mindset that as adults they will need to work for money. After the program (introduction, intermediate and advanced), they will have a mindset that money should work for them. That is – the way to wealth is through cash flowing assets.
+                                Before joining the programs, kids and teens normally have a mindset that as adults they will need to work for money. For example, “I wish to be a pilot”. 
+                                After the program, they will have a mindset that money should work for them. For example, “I wish to be a pilot that owns an airline which employs other pilots”. 
+                                That is – the way to wealth is through cash flowing assets.
                             </p>
                         </div>
                     </ol>
