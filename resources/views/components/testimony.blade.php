4dcd37d4591216127e9e132765bfc2d65a48ff6f
anthony-baru <anthonybaru@gmail.com>
2020-06-20 14:30:16 +0300

    image src modified

--- resources/views/components/testimony.blade.php
+++ resources/views/components/testimony.blade.php
@@ -92,10 +92,10 @@
                                         <h2 class="uk-modal-title">Christopher Kitonga– Parent at Kiuini Preparatory School</h2>
                                         <p>
                                             I remember the day my 8 year old daughter asked me if the house we live in was an asset or a liability… <br>
-                                            After absorbing the shock; I asked her if she knew what she was talking about. To my surprise, she was able to articulate them. I was impressed! I responded to her question by saying it was an asset. She followed through by asking if we were paying any amount for it. As my mind wondered where she was going, I told her we do pay the bank some money every month. “You mean mortgage?”… She said. I stared at her in surprise and before I could say a word, she added, “Then it’s a liability because it moves money out of your pocket”. Even with an Accounting background, my jaws dropped! 
+                                            After absorbing the shock; I asked her if she knew what she was talking about. To my surprise, she was able to articulate them. I was impressed! I responded to her question by saying it was an asset. She followed through by asking if we were paying any amount for it. As my mind wondered where she was going, I told her we do pay the bank some money every month. “You mean mortgage?”… She said. I stared at her in surprise and before I could say a word, she added, “Then it’s a liability because it moves money out of your pocket”. Even with an Accounting background, my jaws dropped!
                                             In summary, I am surprised and impressed at how Dreamers Club Africa is able to articulate financial terms for kids to understand at their level. The activities given for discussion with parents have really deepened my relationship with her. It’s a joyful feeling to see her gain these incredible knowledge and skills around financial literacy at her age, which l wish l had.
                                         </p>
-                                        
+
                                     </div>
                                 </div>
                                 <p class="name"><i>Christopher Kitonga– Parent at Kiuini Preparatory School</i></p>
@@ -121,7 +121,7 @@
                                         <p>
                                             Teaching financial skills to young children will help them navigate through life wisely. They need to understand that money is a tool and if used well, they are sure to succeed. I have watched in amazement Dreamers Club Africa help our children at Mavuno Church, not just learn and apply essential financial skills such as savings, setting goals, breaking them down into actionable steps and tracking their progress, but also help them realize that they can turn their passion into profit. Dreamers Club Africa has done a great job.
                                         </p>
-                            
+
                                     </div>
                                 </div>
                                 <p class="name"><i>Pastor Nzilani– Children Pastor at Mavuno Church Hillside</i></p>
@@ -152,7 +152,7 @@
                                             We had also two entrepreneurship completion at CAP among centers within Nairobi Region. Out of the two we scoped position 1 which was something our center had never managed. The business ideas were so unique and solving social real problem e.g. an app to improve network at rural areas, an app to reduce road accidents by detecting seat belts that were off, bio fuel among other concepts. Our students left with a very different mindset and ready to tackle the economic hardship. We are very grateful to have such an amazing mentor who deeply cares for the next generation.
 
                                         </p>
-                            
+
                                     </div>
                                 </div>
                                 <p class="name"><i>Godwin Makokha – Manager CAP Youth Empowerment Institute (Athi River)</i></p>
@@ -162,7 +162,7 @@
 
                     <div class="item">
                         <div class="testimony-wrap d-flex">
-                            <div class="user-img mr-4" style="background-image: url({{ asset('assets/images/testimonials/Octavia.png') }})">
+                            <div class="user-img mr-4" style="background-image: url({{ asset('assets/images/testimonials/Octavia.jpg') }})">
                             </div>
                             <div class="text ml-2 bg-light">
                                 <span class="quote d-flex align-items-center justify-content-center">
@@ -179,13 +179,13 @@
                                         <p>
                                             I just love Dreamers Club!
                                             Our teacher has brought fun in learning about money management and entrepreneurship. We often play games to help us understand and focus on what we are taught.
-                                            We are given tasks to help us understand responsibilities and sacrifice. I never realized how much of a liability I was until I learnt the value of money. 
+                                            We are given tasks to help us understand responsibilities and sacrifice. I never realized how much of a liability I was until I learnt the value of money.
                                             I now understand why spending should be limited. Any time I earn, the first thing is set aside some amount – save before spending. I have learnt how to compare prices of goods and services when I accompany my mum for shopping.
-                                            We have been taught how to start and develop businesses through problem solving and making use of our skills. 
-                                            I have been able to save money from doing chores and from my painting business. It gives me much satisfaction to pay for what I want from the money I make. 
+                                            We have been taught how to start and develop businesses through problem solving and making use of our skills.
+                                            I have been able to save money from doing chores and from my painting business. It gives me much satisfaction to pay for what I want from the money I make.
 
                                         </p>
-                            
+
                                     </div>
                                 </div>
                                 <p class="name"><i>Octavia – Student at Mavuno Church Hillside </i></p>
@@ -195,7 +195,7 @@
 
                     <div class="item">
                         <div class="testimony-wrap d-flex">
-                            <div class="user-img mr-4" style="background-image: url({{ asset('assets/images/testimonials/Trevor.png') }})">
+                            <div class="user-img mr-4" style="background-image: url({{ asset('assets/images/testimonials/Trevor.jpg') }})">
                             </div>
                             <div class="text ml-2 bg-light">
                                 <span class="quote d-flex align-items-center justify-content-center">
@@ -214,7 +214,7 @@
                                             In a very short span after joining Dreamers Club Africa at Mavuno Church, I was inspired to make changes and create actionable plans for my financial future. I turned my passion of acting into a business and have made some significant savings from this. I have set a goal beyond acting to owning a production company before I turn 25 years. Dreamers Club Africa is supporting me to achieve my goal.
                                             On the subject of money management, I have seen how I can easily achieve my future financial freedom by following a simple and smart rule we were taught: “Money spent on liabilities should come from assets and not from income.” Any income should first be used to build assets and then the money coming from these assets is what should be used to purchase liabilities. Makes sense…
                                         </p>
-                            
+
                                     </div>
                                 </div>
                                 <p class="name"><i>Trevor Johnes – Student at Mavuno Church Hillside </i></p>
@@ -241,7 +241,7 @@
                                         <p>
                                             In all honesty, I wish I had learnt the many valuable lessons Dreamers Club Africa taught me when I was a little boy. When I joined CAP Youth Institute, my dream was to work for a 5 star hotel. I will never forget these words from the Dreamers Club Founder. “You don’t really understand how much you limit yourself using your mind. Why dream of being a doctor when you can dream of owning a massive hospital and remain a doctor? Why dream of being a pilot when you can dream of owning an airline with a fleet of aircrafts and remain a pilot? Why dream of being a chef when you can dream of owning a 5 star hotel and remain a chef? Why dream of being a musician when you can dream of owning a recoding and producing company and remain a musician? ...” How powerful is that? When I left CAP, I had elevated my dream to owning a brand name of hotels and cottages. Currently, I am working at Galaxy Resort in Kitengela. I have achieved my child hood dream, but it is just the begging of a detailed plan and actions towards my revised dream.
                                         </p>
-                            
+
                                     </div>
                                 </div>
                                 <p class="name"><i>Levis Irungu – Former CAP Youth Institute Student </i></p>
