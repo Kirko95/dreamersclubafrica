79d478cae78e7e6624f3dc7e711a65175b5963e1
peterchege442@gmail.com <peterchege442@gmail.com>
2020-06-20 14:35:12 +0300

    add another child

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -40,7 +40,38 @@
                         <label for="exampleFormControlInput1">Phone Number</label>
                         <input name="phone" type="tel" class="form-control" id="" placeholder=" e.g 07xx 342 xxx" required>
                     </div>
+                </div>
+                <br><hr><br>
+                
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="text-left ">Child's details: </h3>
+                    </div>
                     
+                    <div class="form-group col-md-6">
+                        <label for="fname">First Name</label>
+                        <input name="parent" type="text" class="form-control" id="" placeholder="e.g john" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="lname">Last name</label>
+                        <input name="child" type="text" class="form-control" id="" placeholder=" e.g doe" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="exampleFormControlInput1">Email</label>
+                        <input name="email" type="email" class="form-control" id="" placeholder=" e.g john@gmail.com" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="exampleFormControlInput1">Phone Number</label>
+                        <input name="phone" type="tel" class="form-control" id="" placeholder=" e.g 07xx 342 xxx" required>
+                    </div>
+                    <div class="col-6">
+                    <button type="submit" class="btn btn-primary px-4 py-3 mt-3 size-btn register-button">Add another child</button>
+                </div>
+                </div>
+                <br><hr><br>
+                
+                <div class="row">
+
                     <div class="form-group col-12">
                         <label for="exampleFormControlSelect1">Is your child intrested in finance</label>
                         <select name="isInterestedInFinance" class="form-control" id="exampleFormControlSelect1" required>
